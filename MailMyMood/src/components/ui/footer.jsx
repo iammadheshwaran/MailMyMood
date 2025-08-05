35bb8e709728547c40c0ee30656ee45cd69d71f1
iammadheshwaran <m.madheshwaran21@gmail.com>
2025-08-05 20:42:31 +0530

    Fix: add Vite as dev dependency

---
+++ MailMyMood/src/components/ui/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <><footer className="text-center text-sm text-gray-500 mt-10 pb-4">
+  Made with ❤️ by <a href="https://your-website.com" target="_blank" className="underline hover:text-gray-700">Marsh</a>
+</footer>
+</>
+  )
+}
+
+export default footer
